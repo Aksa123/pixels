7b2e50a72bc90d83660ba1a865aa3b0003b23d64
Aksa123 <=>
2020-07-06 08:49:07 +0700

    remove useless img

--- exp.py
+++ exp.py
@@ -24,8 +24,3 @@
 #     return wrapper_func
 
 
-sql = "sum(calculation) as total_points from user_points_histories where action in ('point_acquire', 'survey_point_acquire') user_id = 336532"
-sql2 = "select id, action, calculation, outlet_id, created_at from user_points_histories where action in ('reward_exchange', 'point_expired') user_id = 336532 order by created_at asc"
-for i in sql2:
-    remain = sql - i.calculation
-    
